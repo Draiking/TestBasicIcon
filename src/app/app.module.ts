a720f82a82211d28837d9831284150e120cf98fe
Gennadya <heavyyrain3039@gmail.com>
2020-12-04 19:07:40 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { IconsComponent } from './icons/icons.component';
+import { SafeHtml } from './pipe/pipeIcon';
+import { SearchPipe } from './pipe/search.pipe';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent,
+    IconsComponent,
+    SafeHtml,
+    SearchPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/icons/icons.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { BASIC_ICONS } from '../basic_icons';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-icons',
+  templateUrl: './icons.component.html',
+  styleUrls: ['./icons.component.css']
+})
+export class IconsComponent implements OnInit {
+
+  icons = []
+  currentSize = 0
+  search:string;
+  resultIcons = {}
+  keys = []
+
+  constructor() { }
+
+  ngOnInit() {
+    _.each(BASIC_ICONS,(icon, index) => {
+      if (icon.indexOf('width') === -1) {
+        const re = /<svg/;
+        icon =  icon.replace(re, '<svg width="20"')
+      }
+      const re = /width="[0-9]*(px)?"/i;
+      const found = icon.match(re);
+      let res = 0
+      if (found && found.length > 0) {
+        res = +(found[0].split('=')[1].replace('"', '').replace('"', '').replace('px', ''))
+      } 
+      this.icons.push({
+        title: index,
+        src: icon,
+        width: res
+      });
+    })
+
+     _.each(this.icons,(icon) => {
+       
+       if(!this.resultIcons[icon.width] || (this.resultIcons[icon.width] && !this.resultIcons[icon.width].icons) ) {
+        this.resultIcons[icon.width] = {
+          icons: [],
+          title: icon.width
+        }
+        this.keys.push(icon.width)
+       }
+       this.resultIcons[icon.width].icons.push(icon)
+    })
+
+    this.keys.sort((a, b) => {
+      return a - b;
+    });
+  }
+
+  changeColor(color: string) {
+    // let svgs = document.getElementsByTagName('svg')
+    let paths = document.getElementsByTagName('path')
+    // _.each(svgs, (el) => {
+    //   el.style.fill = color
+    // })
+    _.each(paths, (el) => {
+      el.style.fill = color
+    })
+  }
+
+}
